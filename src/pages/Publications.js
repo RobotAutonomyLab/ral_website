9b1bc868b84b6af35c7e21923faad788758e06a0
Ilyas <ilyas.esack.dawoodjee@gmail.com>
2026-01-28 16:51:39 -0600

    update

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -230,7 +230,7 @@ function Publications() {
                                                                     </a>
                                                                 ) : link.type === "PDF" ? (
                                                                     <a
-                                                                        href={process.env.PUBLIC_URL + link.url.replace('/public', '')}
+                                                                        href={link.url.replace('/public', '')}
                                                                         key={`${link.type}-${index}`}
                                                                         target="_blank"
                                                                         rel="noreferrer"
